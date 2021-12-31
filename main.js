84b492114efb9460cdd4f4b2f9013f98118b55f4
masudursourav <masudursourav@ieee.org>
2021-12-31 11:30:36 +0600

    for dekstop

---
+++ main.js
@@ -0,0 +1,18 @@
+const removeTransition = (key)=>{
+    if(key.propertyName !== 'transform') return;
+    key.target.classList.remove('playing');
+ }
+const playSound = (e) =>{
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"`);
+    const key = document.querySelector(`div[data-key = "${e.keyCode}"`);
+    if(!audio) return;
+    
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => {
+key.addEventListener('transitionend',removeTransition);    
+});
+window.addEventListener('keydown',playSound);
